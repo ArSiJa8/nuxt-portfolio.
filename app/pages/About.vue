5a2aaf56b8d1a217f63c3aaf71ca17db6441fba9
ArSiJa8 <silvan@arsija.net>
2026-02-21 20:56:14 +0100

    added About.vue

---
+++ app/pages/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+  pageTransition: {
+    name: 'slide-right',
+    mode: 'out-in'
+  }
+})
+</script>
+<template>
+  <main>
+    <AboutMe />
+  </main>
+</template>

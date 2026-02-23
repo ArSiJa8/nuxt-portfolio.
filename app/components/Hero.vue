9ed360a93ebae3f932267b287896a5f989493269
ArSiJa8 <silvan@arsija.net>
2026-02-23 14:45:27 +0100

    fixed minor things

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -2,7 +2,7 @@
 <div class="center-screen">
     <div class="card">
         <h1>ArSiJa</h1>
-        <p>Blender Animations & Thumbnail Creation</p>
+        <p>👋 Hi! - I make Websites - Vue/Nuxt</p>
     </div>
 </div>
 </template>

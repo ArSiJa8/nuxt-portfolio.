b10199238dc9e6bd2518320f6f6d78ffaa1271ea
ArSi <silvan@arsija.net>
2026-03-09 09:41:51 +0100

    Update Blog.vue

--- app/pages/Blog.vue
+++ app/pages/Blog.vue
@@ -1,3 +1,15 @@
+<script setup>
+useSeoMeta({
+  title: 'ArSiJa Portfolio | Blog',
+  description: 'My Web Dev Blog about the newest Updates in Tech',
+  ogTitle: 'ArSiJa Portfolio | Blog',
+  ogDescription: 'MyWeb Dev Blog about the newest Updates in Tech',
+  
+})
+  
+</script>
+
+
 <template>
   <BlocC />
 </template>

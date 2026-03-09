ed7bac0f6cf425e0bdfb4d9639439a6801570cf9
ArSi <silvan@arsija.net>
2026-03-09 09:39:08 +0100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-
+useSeoMeta({
+  title: 'ArSiJa Portfolio | Home',
+  description: 'About Me, my Skills and Projects.',
+  ogTitle: 'ArSiJa Portfolio | Home',
+  ogDescription: 'About Me, my Skills and Projects.',
+  
+})
 import Scrolltest from "~/components/Scrolltest.vue";
 </script>
 

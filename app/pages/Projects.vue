f77084d4919d0c7a11415d1f92002ef60c03acba
ArSi <silvan@arsija.net>
2026-03-09 12:24:00 +0100

    Update Projects.vue

--- app/pages/Projects.vue
+++ app/pages/Projects.vue
@@ -1,3 +1,13 @@
+<script setup>
+useSeoMeta({
+  title: 'ArSiJa Portfolio | Projects',
+  description: 'My Favourite Projects.',
+  ogTitle: 'ArSiJa Portfolio | Projects',
+  ogDescription: 'My Favourite Projects.',
+  
+})
+</script>
+
 <template>
   <div>
     <ProjectsC />
